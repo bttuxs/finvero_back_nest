63e57960d2fb682c7a1a593d0690a39c37dd8363
Baldomero de castilla <bttuxs@gmail.com>
2023-10-23 20:40:40 -0600

    Configuracion de modulo app-orden

--- apps/app-orden/src/app-orden.module.ts
+++ apps/app-orden/src/app-orden.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { OrdenModule } from './app-orden/orden/orden.module';
+import { ConfigModule } from '@nestjs/config';
+import { configMysql } from '@app/configs';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+      cache: true,
+    }),
+    configMysql,
+    OrdenModule,
+  ],
   controllers: [],
   providers: [],
 })
