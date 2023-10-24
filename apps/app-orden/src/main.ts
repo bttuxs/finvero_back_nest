f05389db26521f3b548ddce49dacf3351cdea701
Baldomero de castilla <bttuxs@gmail.com>
2023-10-23 20:42:38 -0600

    Configuracion de modulo app-orden

--- apps/app-orden/src/main.ts
+++ apps/app-orden/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppOrdenModule } from './app-orden.module';
+import { LoggerService } from '@app/configs/logger.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppOrdenModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppOrdenModule, {
+    logger: new LoggerService(),
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3002);
 }
 bootstrap();
